d6247681cb861b823ebcd5a4f62a3b4bcae52f7c
skulegirl <anna@egate.net>
2016-08-18 14:11:08 -0400

    Change form to be shown within a view, rather than on a separate page, and reload the previous filters in it. Reorganize some of the order of the filters, and add a few dummies to make life easier. * Make the options fieldset collapsible for cleanliness * Move the javascript for remembering zoom into this module from the theme. * Move the css for styling the output into this module from the theme.

---
+++ js/gmap_remember_zoom.js
@@ -0,0 +1,46 @@
+/******
+ * create cookie to remember our center and zoom level when we 
+ * navigate back to the map page
+ * */
+
+(function ($) {
+  Drupal.behaviors.gmapsavepos = { 
+    attach: function(context, settings) {
+
+      if (typeof Drupal.gmap != 'undefined') {
+
+        Drupal.gmap.addHandler('gmap', function(elem) {
+          var obj = this;
+          obj.bind("maptypechange", function () {
+            var map = obj.map;
+            window.setTimeout(function() {
+              var cookiename = 'gmapsavedpos_' + location.pathname;      
+              var pos = $.cookie(cookiename);
+              if (pos != null) {
+                var posJSON = JSON.parse(pos);
+                map.setCenter(new google.maps.LatLng(posJSON.latitude, posJSON.longitude));
+                map.setZoom(posJSON.zoom);
+              }
+              $('body').addClass('gmapsavedpos');
+            }, 2000);
+          });
+
+          function saveposition() {
+            if ($('body').hasClass('gmapsavedpos')) {
+              var map = obj.map;
+              var centerJSON = {
+                'latitude': obj.vars.latitude,
+                'longitude': obj.vars.longitude,
+                'zoom': obj.vars.zoom
+              };
+              var cookiename = 'gmapsavedpos_' + location.pathname;      
+              $.cookie(cookiename, JSON.stringify(centerJSON), {expire: 1});
+            }
+          } 
+          obj.bind('move', saveposition);
+          obj.bind('zoom', saveposition); 
+        });
+      }
+    }
+  };
+})(jQuery);
